3a2d1a737e7a86b9fb39bd42167fcb572ea45b29
[YOUNES ADI] <younesadi18@gmail.com>
2022-03-31 19:54:00 +0100

    finished-3

--- Home_work/HW.js
+++ Home_work/HW.js
@@ -1,10 +1,18 @@
 //with arrays
 function calculate(N){
-    let U=[0,1,2];
-    for(let i=3;i<=N;i++){
-        let x=U.push(U[i]=2*U[i-1]+3*U[i-3]);    
-    }
+    if(N==0){
+        return 0;
+    }else if(N==1){
+        return 1;
+    }else if(N==2){
+        return 2;
+    }else{    
+            let U=[0,1,2];
+            for(let i=3;i<=N;i++){
+            let x=U.push(U[i]=2*U[i-1]+3*U[i-3]);    
+                                 }
     return U[N];
+    }
 }
 //with Recursion
 function cal_rec(N){
@@ -19,5 +27,5 @@ function cal_rec(N){
     }
 }
 
-console.log("with arrays = "+calculate(5));
-console.log("with Recursion = "+cal_rec(5));
+console.log("with arrays = "+calculate(2));
+console.log("with Recursion = "+cal_rec(2));
