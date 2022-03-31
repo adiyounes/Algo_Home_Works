decd39451620d772b34330256af05b0f2c001c4d
[YOUNES ADI] <younesadi18@gmail.com>
2022-03-31 19:42:48 +0100

    finished

--- Home_work/HW.js
+++ Home_work/HW.js
@@ -6,4 +6,17 @@ function calculate(N){
     return U[N];
 }
 
+function cal_rec(N){
+    if(N==0){
+        return 0;
+    }else if(N==1){
+        return 1;
+    }else if(N==2){
+        return 2;
+    }else{
+        return 2*cal_rec(N-1)+3*cal_rec(N-3);
+    }
+}
+
 console.log(calculate(5));
+console.log(cal_rec(5));
