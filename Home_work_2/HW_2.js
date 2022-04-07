c02badffaa7aedfc950f4830ecbcd61ae3fcb315
[YOUNES ADI] <younesadi18@gmail.com>
2022-04-07 10:32:10 +0100

    homework2

---
+++ Home_work_2/HW_2.js
@@ -0,0 +1,34 @@
+function inverLongSeq(arr){
+    if(arr.length<2){
+        return arr;
+    }
+    let i=0;
+    let seq=0;
+    let j=0;
+    while(i<arr.length-1){
+        let s=0;
+        while(arr[i]<arr[i+1]){
+            s++;
+            i++;
+        }
+        if(s>seq){
+            seq=s;
+            j=i-s;
+        }
+        i++;
+    }
+    return inverse(arr,j,j+seq);
+}
+function inverse(arr,p,q){
+    var mid =Math.ceil((p+q)/2);
+    for(;p<mid;p++,q--){
+        [arr[p],arr[q]]=[arr[q],arr[p]];
+    }
+    return arr;
+}
+
+var array=[2,3,4,15,16,17,19,20,0,0,-4,5,6,7,8,9,10,1,2];
+var array2=[1,2]
+var array3=[];
+var array4=[2,3,4,15,16,17,19,20,0,0,-4,5,6,7,8,9,10,11,12,13,14,15,16];
+console.log(inverLongSeq(array));
